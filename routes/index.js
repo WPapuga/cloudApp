71e963838cc9057fbc8d828ff6cb472fc710967d
WPapuga <Wojtek.Papuga@yahoo.com>
2023-04-19 17:18:06 +0200

    dodaj zmiany

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 
-const CLIENT_ID = OAuth2Data.client.id;
-const CLIENT_SECRET = OAuth2Data.client.secret;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URL = OAuth2Data.client.redirect
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
