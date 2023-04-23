82f613332723b75a709a6e2cfeca55f20ec63d8b
WPapuga <Wojtek.Papuga@yahoo.com>
2023-04-23 15:53:32 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,28 +7,28 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 
-app.use(session({
-  CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  REDIRECT_URL: OAuth2Data.client.redirect,
-  oAuth2Client: new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL),
-  authed: false
-}))
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URL = OAuth2Data.client.redirect;
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+const authed = false;
+
 
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'index.html');
   res.sendFile(filePath);
 })
 app.get('/login', function(req, res){
-  if (!req.session.authed) {
-    const url = req.session.oAuth2Client.generateAuthUrl({
+  if (!authed) {
+    const url = oAuth2Client.generateAuthUrl({
       access_type: 'offline',
       scope: 'https://www.googleapis.com/auth/userinfo.profile'
     });
     console.log(url)
     res.redirect(url);
   } else {
-    var oauth2 = google.oauth2({ auth: req.session.oAuth2Client, version: 'v2'});
+    var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2'});
     oauth2.userinfo.v2.me.get(function(err, result) {
       if(err) {
         console.log("Błąd");
@@ -47,8 +47,8 @@ app.get('/login', function(req, res){
   };
 })
 app.get('/logout', function(req, res) {
-  var oauth2 = google.oauth2({ auth: req.session.oAuth2Client, version: 'v2'});
-  req.session.authed = false;
+  var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2'});
+  authed = false;
   res.send(`<p>Wylogowano</p>
             <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>`
   );
@@ -56,14 +56,14 @@ app.get('/logout', function(req, res) {
 app.get('/auth/google/callback', function (req, res) {
     const code = req.query.code
     if (code) {
-        req.session.oAuth2Client.getToken(code, function (err, tokens) {
+        oAuth2Client.getToken(code, function (err, tokens) {
             if (err) {
                 console.log('Error authenticating')
                 console.log(err);
             } else {
                 console.log('Successfully authenticated');
-                req.session.oAuth2Client.setCredentials(tokens);
-                req.session.authed = true;
+                oAuth2Client.setCredentials(tokens);
+                authed = true;
                 res.redirect('/')
             }
         });
