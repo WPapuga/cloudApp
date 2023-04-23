27301d9aad68260cd90063c5936b0d354cae43b1
WPapuga <Wojtek.Papuga@yahoo.com>
2023-04-23 17:25:50 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,24 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 
+
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URL = OAuth2Data.client.redirect
 
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
 var authed = false;
+
+const CLIENT_ID_GH = process.env.CLIENT_ID_GH;
+const CLIENT_SECRET_GH = process.env.CLIENT_SECRET_GH;
+const gh_link = `https://github.com/login/oauth/authorize?client_id=` + CLIENT_ID_GH + 
+                `&redirect_uri=https://cloudappwp.azurewebsites.net/auth/github/callback`
+                `&scope=read:user+user:email` + 
+                `&allow_signup=true`;
+var authed_gh;
+var token_gh;
+
+
 
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'index.html');
@@ -44,9 +56,25 @@ app.get('/login', function(req, res){
     });
   };
 })
+app.get('/loginGH', function(req, res) {
+  if(!authed_gh){
+    res.redirect(gh_link);
+  } else {
+    res.send(`<body>
+                  <p> Zalogowano przez GitHub! </p>
+                  <button id="logoutButton" onclick="window.location.href = '/logoutGH';">Wyloguj się</button>
+                  <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>
+              </body>`)
+  }
+});
 app.get('/logout', function(req, res) {
-  var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2'});
   authed = false;
+  res.send(`<p>Wylogowano</p>
+            <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>`
+  );
+});
+app.get('/logoutGH', function(req, res) {
+  authed_gh = false;
   res.send(`<p>Wylogowano</p>
             <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>`
   );
@@ -63,10 +91,33 @@ app.get('/auth/google/callback', function (req, res) {
                 oAuth2Client.setCredentials(tokens);
                 authed = true;
                 res.send(`<p>Zalogowano</p>
-                          <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>`)
+                          <button id="returnButton" onclick="window.location.href = '/';">Powrót do strony głównej</button>`)
             }
         });
     }
+});
+
+async function getToken(code){
+  const { data } = await axios({
+    url: 'https://github.com/login/oauth/access_token',
+    method: 'get',
+    params: {
+      client_id: process.env.CLIENT_ID_GH,
+      client_secret: process.env.CLIENT_SECRET_GH,
+      redirect_uri: 'https://cloudappwp.azurewebsites.net/auth/github/callback',
+      code,
+    },
+  });
+  return data.access_token;
+}
+app.get('/auth/github/callback', function (req, res) {
+  const code = req.query.code;
+  if (code) {
+    token_gh = getToken(code);
+    res.send(`<p>Zalogowano</p>
+              <button id="returnButton" onclick="window.location.href = '/';">Powrót do strony głównej</button>`);
+  }
+
 });
 
 const port = 5000
