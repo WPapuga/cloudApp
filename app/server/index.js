f031026c97f50965fda27bc10e44825af185f6b2
WPapuga <Wojtek.Papuga@yahoo.com>
2023-05-06 20:53:58 +0200

    big changes

--- app/server/index.js
+++ app/server/index.js
@@ -1,39 +1,21 @@
 const { google } = require('googleapis')
 const express = require('express')
-const { Client } = require('pg')
-const OAuth2Data = require('../google_key.json')
+const OAuth2Data = require('./google_key.json')
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const axios = require('axios');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const pg = require("pg")
+require('dotenv').config()
+
 const app = express()
-
-const connectDB = async () => {
-  try {
-    const client = new Client({
-      user: process.env.PGUSER,
-      host: process.env.PGHOST,
-      database: process.env.PGDATABASE,
-      password: process.env.PGPASSWORD,
-      port: process.env.PGPORT
-    })
-
-    await client.connect()
-    const res = await client.query('SELECT * FROM users')
-    console.log(res)
-    await client.end()
-  } catch (error){
-    console.log(error)
-  }
-}
-
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URL = OAuth2Data.client.redirect
-
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
-var authed = false;
-
+var authed = false
 const CLIENT_ID_GH = process.env.CLIENT_ID_GH;
 const CLIENT_SECRET_GH = process.env.CLIENT_SECRET_GH;
 const gh_link = `https://github.com/login/oauth/authorize?client_id=` + CLIENT_ID_GH + 
@@ -42,13 +24,36 @@ const gh_link = `https://github.com/login/oauth/authorize?client_id=` + CLIENT_I
                 `&allow_signup=true`;
 var authed_gh;
 var token_gh;
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env
+const credentialsDB = {
+  user: PGUSER,
+  host: PGHOST,
+  database: PGDATABASE,
+  password: PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: true
+}
+const pool = new pg.Pool(credentialsDB)
+
+
+const testDB = async () => {
+  try {
+      const res = await pool.query('SELECT * FROM users')
+      console.log(res)
+  } catch (error) {
+      console.log(error)
+  }
+}
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"))
 
 
 
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
-  res.sendFile(filePath);
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// })
+
 app.get('/login', function(req, res){
   if (!authed) {
     const url = oAuth2Client.generateAuthUrl({
@@ -56,7 +61,8 @@ app.get('/login', function(req, res){
       scope: 'https://www.googleapis.com/auth/userinfo.profile'
     });
     console.log(url)
-    res.redirect(url);
+
+
   } else {
     var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2'});
     oauth2.userinfo.v2.me.get(function(err, result) {
@@ -76,6 +82,7 @@ app.get('/login', function(req, res){
     });
   };
 })
+
 app.get('/loginGH', function(req, res) {
   if(!authed_gh){
     res.redirect(gh_link);
@@ -87,18 +94,21 @@ app.get('/loginGH', function(req, res) {
               </body>`)
   }
 });
+
 app.get('/logout', function(req, res) {
   authed = false;
   res.send(`<p>Wylogowano</p>
             <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>`
   );
 });
+
 app.get('/logoutGH', function(req, res) {
   authed_gh = false;
   res.send(`<p>Wylogowano</p>
             <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>`
   );
 });
+
 app.get('/auth/google/callback', function (req, res) {
     const code = req.query.code
     if (code) {
@@ -130,6 +140,7 @@ async function getToken(code){
   });
   return data.access_token;
 }
+
 app.get('/auth/github/callback', function (req, res) {
   const code = req.query.code;
   if (code) {
---
+++ app/src/Navbar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'; 
+import "./Navbar.css";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
+
+function Greeting(props) {
+  const navigate = useNavigate();
+  const handleButtonClickGoogle = () => {
+    navigate('/login', { replace: true });
+    window.location.reload(); 
+  };
+  const handleButtonClickGitHub = () => {
+    navigate('/loginGH', { replace: true });
+    window.location.reload(); 
+  };
+  return ( 
+      <div className="Navbar">
+      <div className="NavLeftSide">
+          <Link to="/">Cloud App WP</Link>
+      </div>
+      <div className="NavLinks">
+          <CustomLink to="/">Strona Główna</CustomLink>
+          <CustomLink to="/">Wyposażenie</CustomLink>
+          <CustomLink to="/">Ryby</CustomLink>
+          {sessionStorage.getItem('isLogged') == "true" ? <CustomLink to="/">Moje Akwaria</CustomLink> : <div></div>}
+      </div>
+      <div className="NavSigning">
+          {sessionStorage.getItem('isLogged') == "true" ? 
+              <CustomLink to="/">Wyloguj</CustomLink>
+              :
+              <>
+              <button onClick={handleButtonClickGoogle}>Zaloguj się Google</button>
+              <button onClick={handleButtonClickGitHub}>Zaloguj się Github</button>
+              </>
+          }
+      </div>
+      </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <Greeting isLoggedIn={true} />
+  )
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+  return (
+    <div className={isActive ? "active" : ""}>
+          <Link to={to} {...props}>{children}</Link>
+    </div>
+  )
+}
+
+export default Navbar
