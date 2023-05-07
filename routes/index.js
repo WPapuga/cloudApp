3f2c8e18e89d825aa8caf918a3ed2523940a2991
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-05-07 21:35:54 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -81,47 +81,31 @@ app.get('/login', function(req, res){
     console.log(URL)
     res.redirect(URL);
   } else {
-    var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2'});
-    oauth2.userinfo.v2.me.get(function(err, result) {
-      if(err) {
-        console.log("Błąd");
-        console.log(err);
-      } else {
-        loggedUser = result.data.name;
-        console.log(loggedUser);
-      }
-      var ret = `<body>
-                      <p> Logged in:` + loggedUser +` <img src="` + result.data.picture + `"height="23" width="23"> </p>
-                      <button id="logoutButton" onclick="window.location.href = '/logout';">Wyloguj się</button>
-                      <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>
-                </body>`;
-      res.send(ret);
-    });
+    const filePath = path.join(__dirname, 'index.html');
+    res.sendFile(filePath);
   };
-})
+});
+
 app.get('/loginGH', function(req, res) {
   if(!authed_gh){
     res.redirect(gh_link);
   } else {
-    res.send(`<body>
-                  <p> Zalogowano przez GitHub! </p>
-                  <button id="logoutButton" onclick="window.location.href = '/logoutGH';">Wyloguj się</button>
-                  <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>
-              </body>`)
+    const filePath = path.join(__dirname, 'index.html');
+    res.sendFile(filePath);
   }
 });
+
 app.get('/logout', function(req, res) {
   authed = false;
-  res.send(`<p>Wylogowano</p>
-            <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>`
-  );
+  const filePath = path.join(__dirname, 'index.html');
+  res.sendFile(filePath);
 });
 app.get('/logoutGH', function(req, res) {
   authed_gh = false;
-  res.send(`<p>Wylogowano</p>
-            <button id="returnButton" onclick="window.location.href = '/';">Powrór do strony głównej</button>`
-  );
+  const filePath = path.join(__dirname, 'index.html');
+  res.sendFile(filePath);
 });
+
 app.get('/auth/google/callback', function (req, res) {
     const code = req.query.code
     if (code) {
@@ -143,8 +127,8 @@ app.get('/auth/google/callback', function (req, res) {
                     console.log(loggedUser);
                   }
                   addUser(loggedUser);
-                  res.send(`<p>Zalogowano</p>
-                            <button id="returnButton" onclick="window.location.href = '/';">Powrót do strony głównej</button>`)
+                  const filePath = path.join(__dirname, 'loggedGoogle.html');
+                  res.sendFile(filePath);
                 });
                 
             }
@@ -171,8 +155,9 @@ app.get('/auth/github/callback', function (req, res) {
   if (code) {
     token_gh = getToken(code);
     authed_gh = true;
-    res.send(`<p>Zalogowano</p>
-              <button id="returnButton" onclick="window.location.href = '/';">Powrót do strony głównej</button>`);
+    addUser('tempGHuser');
+    const filePath = path.join(__dirname, 'loggedGithub.html');
+    res.sendFile(filePath);
   }
 
 });
