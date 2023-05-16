3834694fdc08d63b50f4b51de22df5f0fff0c56c
WPapuga <Wojtek.Papuga@yahoo.com>
2023-05-17 00:01:21 +0200

    zadanie 7

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const { google } = require('googleapis')
 const express = require('express')
-const bodyParser = require('body-parser');
 const pg = require('pg')
 const http = require('http');
 const url = require('url');
@@ -15,7 +14,7 @@ const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URL = 'https://cloudappwp.azurewebsites.net/auth/google/callback'
 const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
-var authed = true
+var authed = false
 var loggedUser;
 const CLIENT_ID_GH = process.env.CLIENT_ID_GH;
 const CLIENT_SECRET_GH = process.env.CLIENT_SECRET_GH;
@@ -71,13 +70,10 @@ function addUser(username) {
   })
 }
 
-app.use(bodyParser.json());
-
 app.get('/', (req, res) => {
   const filePath = path.join(__dirname, 'index.html');
   res.sendFile(filePath);
 })
-
 app.get('/login', function(req, res){
   if (!authed) {
     const URL = oAuth2Client.generateAuthUrl({
@@ -191,125 +187,6 @@ app.get('/api/getUsername', function (req, res) {
   console.log(authed + " " + authed_gh);
   if( authed || authed_gh) {
     res.send({username: loggedUser});
-  } else {
-    res.send("Nie jesteś zalogowany");
-  }
-});
-
-app.get('/api/getDBName', function (req, res) {
-  console.log(authed + " " + authed_gh);
-  if( authed || authed_gh) {
-    pool.query('SELECT current_database()', (error, result) => { 
-      if (error) throw error;
-      console.log(result.rows[0].current_database);
-      res.send({dbname: result.rows[0].current_database});
-    });
-  } else {
-    res.send("Nie jesteś zalogowany");
-  }
-});
-
-app.get('/api/getTables', function (req, res) {
-  console.log(authed + " " + authed_gh);
-  if( authed || authed_gh) {
-    pool.query(`SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE'`, (error, result) => { 
-      if (error) throw error;
-      console.log(result.rows);
-      res.send(result.rows);
-    });
-  } else {
-    res.send("Nie jesteś zalogowany");
-  }
-});
-
-app.get('/api/getColumns', function (req, res) {
-  const tableName = req.query.table;
-  if( authed || authed_gh) {  
-    pool.query(`SELECT * FROM information_schema.columns
-                WHERE table_schema = 'public'
-                AND table_name   = '${tableName}'`, (error, result) => { 
-      if (error) throw error;
-      res.send(result.rows);
-    });
-  } else {
-    res.send("Nie jesteś zalogowany");
-  }
-});
-
-
-app.get('/api/getTableData', function (req, res) {
-  const tableName = req.query.table;
-  if( authed || authed_gh) {  
-    pool.query(`SELECT * FROM ${tableName}`, (error, result) => { 
-      if (error) throw error;
-      res.send(result.rows);
-    });
-  } else {
-    res.send("Nie jesteś zalogowany");
-  }
-});
-
-app.post('/api/insertRow', function (req, res) {
-  const tableName = req.body.tableName;
-  const row = req.body.row;
-  if( authed || authed_gh) {  
-    var SQLstmt = `INSERT INTO ${tableName}(`
-    for (var key in row) {
-      SQLstmt += key + ", ";
-    }
-    SQLstmt = SQLstmt.slice(0, -2) + ") VALUES (";;   
-    for (var key in row) {
-      SQLstmt += "'" + row[key] + "', ";
-    }
-    SQLstmt = SQLstmt.slice(0, -2) + ");";; 
-    console.log(SQLstmt);
-    pool.query(SQLstmt, (error, result) => { 
-      if (error) throw error;
-      res.send("Dodano wiersz");
-    });
-  } else {
-    res.send("Nie jesteś zalogowany");
-  }
-});
-
-app.delete('/api/deleteRow', function (req, res) {
-  const tableName = req.body.tableName;
-  const row = req.body.row;
-  if( authed || authed_gh) {  
-    var SQLstmt = `DELETE FROM ${tableName} WHERE`
-    for (var key in row) {
-      SQLstmt += " " + key + " = '" + row[key] + "' AND";
-    }
-    SQLstmt = SQLstmt.slice(0, -4) + ";";;
-    console.log(SQLstmt);
-    pool.query(SQLstmt, (error, result) => { 
-      if (error) throw error;
-      res.send("Usunięto wiersz");
-    });
-  } else {
-    res.send("Nie jesteś zalogowany");
-  }
-});
-
-app.put('/api/editRow', function (req, res) {
-  const tableName = req.body.tableName;
-  const row = req.body.row;
-  const rowToBeEdited = req.body.rowToBeEdited;
-  if( authed || authed_gh) {  
-    var SQLstmt = `UPDATE ${tableName} SET`
-    for (var key in row) {
-      SQLstmt += " " + key + " = '" + row[key] + "',";
-    }
-    SQLstmt = SQLstmt.slice(0, -1) + " WHERE";
-    for (var key in rowToBeEdited) {
-      SQLstmt += " " + key + " = '" + rowToBeEdited[key] + "' AND";
-    }
-    SQLstmt = SQLstmt.slice(0, -4) + ";";;
-    console.log(SQLstmt);
-    pool.query(SQLstmt, (error, result) => { 
-      if (error) throw error;
-      res.send("Zaktualizowano wiersz");
-    });
   } else {
     res.send("Nie jesteś zalogowany");
   }
